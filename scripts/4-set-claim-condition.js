479de62000aa04fb0d4472795ffb79fe7fa09b77
filipeVenancio <filipe.venancio@fixus.co.ao>
2022-01-02 22:49:42 +0000

    repo m00nDAO
    Added manually after finishing Buildspace build a DAO course.

---
+++ scripts/4-set-claim-condition.js
@@ -0,0 +1,23 @@
+import sdk from "./1-initialize-sdk.js";
+
+const bundleDrop = sdk.getBundleDropModule(
+  "0x334f37b59bd0222208f758dF47D41Cd56b477065",
+);
+
+(async () => {
+  try {
+    const claimConditionFactory = bundleDrop.getClaimConditionFactory();
+    // Specify conditions.
+    claimConditionFactory.newClaimPhase({
+      startTime: new Date(),
+      maxQuantity: 50_000,
+      maxQuantityPerTransaction: 1,
+    });
+    
+    
+    await bundleDrop.setClaimCondition(0, claimConditionFactory);
+    console.log("✅ Successfully set claim condition on bundle drop:", bundleDrop.address);
+  } catch (error) {
+    console.error("Failed to set claim condition", error);
+  }
+})()
